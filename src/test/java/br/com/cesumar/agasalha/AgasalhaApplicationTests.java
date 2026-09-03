2f4c3e6d5673859ef14eae3c56d06dd36451d9f8
Joao-Salvalagio <joaosalvalagio@gmail.com>
2026-09-02 23:21:14 -0300

    test: adiciona teste de subida do contexto

---
+++ src/test/java/br/com/cesumar/agasalha/AgasalhaApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.cesumar.agasalha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AgasalhaApplicationTests {
+
+    @Test
+    void contextoSobe() {
+    }
+}
